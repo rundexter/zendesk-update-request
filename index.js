238a6a28e4909d3907078a838ed7f358f3f9ff41
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-12 17:37:25 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+var util = require('./util.js'),
+    _ = require('lodash'),
+    pickInputs = {
+        'id': { key: 'id', validate: { req: true }},
+        'comment': { key: 'comment', validate: { req: true }}
+    }, pickOutputs = {
+        id: 'id',
+        requester_id: 'requester_id',
+        assignee_id: 'assignee_id',
+        collaborator_ids: 'collaborator_ids',
+        created_at: 'created_at',
+        subject: 'subject',
+        description: 'description',
+        status: 'status'
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +23,38 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            token = dexter.provider('zendesk').credentials('access_token'),
+            username = dexter.provider('zendesk').data('username'),
+            subdomain = dexter.provider('zendesk').data('subdomain');
+
+        if (!token || !username || !subdomain)
+            return this.fail('A [access_token, username, subdomain] environment variable is required for this module');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        var request = require('request').defaults({
+            baseUrl: 'https://' + subdomain + '.zendesk.com/api/v2/'
+        });
+
+        request.put({
+            uri: '/requests/' + inputs.id + '.json',
+            body: {
+                request: { comment: { body: inputs.comment } }
+            },
+            auth: {
+                user: username.concat('/token'),
+                pass: token
+            },
+            json: true
+        }, function (err, response, result) {
+            console.log(result);
+            if (err || (result && result.error))
+                this.fail(err || result);
+            else
+                this.complete(util.pickOutputs(result.request, pickOutputs));
+        }.bind(this));
     }
 };
